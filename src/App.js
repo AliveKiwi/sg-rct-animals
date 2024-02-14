e670978f015dfc92891c07dcdc2f1bfc12ab3368
AliveKiwi <prateekjais.dev@gmail.com>
2024-02-15 03:48:08 +0530

    4. Why useState use Array Destructuring instead of Object Destructuring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import { useState } from 'react';
+// import { useState } from 'react';
+
+function useState(defaultValue) {
+  return {
+    yourState: defaultValue,
+    yourSetter: () => {},
+  };
+}
 
 function App() {
-  const [count, setCount] = useState(0);
+  //   const [count, setCount] = useState(0);
+
+  // Why useState use Array Destructuring instead of Object Destructuring?Because it requires more effort
+  const stateConfig = useState(0);
+
+  //   const count = stateConfig.yourState;
+  //   const setCount = stateConfig.yourSetter;
+  const { yourState: count, yourSetter: setCount } = stateConfig;
 
   const handleClick = () => {
     setCount(count + 1);
