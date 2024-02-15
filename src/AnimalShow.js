df946f58e78f8dc3f9408421faac1813b1951321
AliveKiwi <prateekjais.dev@gmail.com>
2024-02-15 14:28:37 +0530

    6. Animal images are added on button click

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,5 +1,26 @@
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+import heart from './svg/heart.svg';
+
+const svgMap = {
+  bird, //=> bird: bird,
+  cat, //=> cat: cat,
+  cow, //=> cow: cow,
+  dog, //=> dog: dog,
+  gator, //=> gator: gator,
+  horse, //=> horse: horse,
+};
+
 function AnimalShow({ type }) {
-  return <div>{type}</div>;
+  return (
+    <div>
+      <img alt="animal" src={svgMap[type]} />
+    </div>
+  );
 }
 
 export default AnimalShow;
